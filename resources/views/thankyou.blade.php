8255edcfedf5477ded217fb78fb20e17896c9f2e
Ahmad Ripai R A <hello.ahmadrifai@gmail.com>
2024-07-19 08:24:06 +0700

    Blade Modify

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -9,6 +9,9 @@
         <h2>Thank you for your order!</h2>
         <p>Your items will be shipped to the provided address. You will receive a tracking number on your phone.</p>
         <p></p>
+
+        <a href="{{route('produk')}}" class="btn btn-light pro" >See More Product</a>
+
     </div>
 </main>
 @endsection
